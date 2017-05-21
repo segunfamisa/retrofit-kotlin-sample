6adfda09de715179377381750a629b38718d0f3a
Segun Famisa <segunfamisa@gmail.com>
2017-05-21 21:53:57 +0100

    [chore] Make request and log response

--- app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/MainActivity.kt
+++ app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.segunfamisa.kotlin.samples.retrofit
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.segunfamisa.kotlin.samples.retrofit.data.SearchRepositoryProvider
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = SearchRepositoryProvider.provideSearchRepository()
+
+        compositeDisposable.add(
+                repository.searchUsers("Lagos", "Java")
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe ({
+                            result ->
+                            Log.d("Result", "There are ${result.items.size} Java developers in Lagos")
+                        }, { error ->
+                            error.printStackTrace()
+                        })
+        )
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
     }
 }
