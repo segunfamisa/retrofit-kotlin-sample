b5b753e3ae78023d4ba80c1b38cbf473a77b780b
Segun Famisa <segunfamisa@gmail.com>
2017-05-21 21:53:19 +0100

    [chore] Modify search query parameter

--- app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/data/SearchRepository.kt
+++ app/src/main/java/com/segunfamisa/kotlin/samples/retrofit/data/SearchRepository.kt
@@ -8,7 +8,7 @@ import io.reactivex.Observable
 class SearchRepository(val apiService: GithubApiService) {
 
     fun searchUsers(location: String, language: String): Observable<Result> {
-        return apiService.search(query = "location:$location+language:$language")
+        return apiService.search(query = "location:$location language:$language")
     }
 
 }
